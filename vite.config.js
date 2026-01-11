460001231d41cbae836d7478c20ce827463b0846
Karthik MR <karthikmr135@gmail.com>
2026-01-11 16:47:29 +0530

    fix: force tailwind compilation

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // <--- MUST BE HERE
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: './', // CRITICAL: This ensures paths work on Vercel
+  plugins: [
+    react(),
+    tailwindcss(), // <--- MUST BE HERE
+  ],
 })
