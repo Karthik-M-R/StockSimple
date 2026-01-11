9aff1e92143e9f45f15aad0f47bd71844720a715
Karthik MR <karthikmr135@gmail.com>
2026-01-11 16:16:07 +0530

    restore original main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,12 @@
-// import React from "react"
-// import ReactDOM from "react-dom/client"
-// import { RouterProvider } from "react-router-dom"
-// import "./index.css";
-
-// import router from "./router/router"
-// import "./App.css"
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <RouterProvider router={router} />
-//   </React.StrictMode>
-// )
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { RouterProvider } from "react-router-dom"
+import router from "./router/router" 
+import "./index.css"
+import "./App.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <div style={{ backgroundColor: 'yellow', height: '100vh', padding: '50px' }}>
-    <h1 style={{ color: 'black' }}>REACT IS WORKING!</h1>
-    <p>If you see this, the problem is in your Router.jsx or CSS imports.</p>
-  </div>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
